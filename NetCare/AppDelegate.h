6a58ad398ba6e9de27bc29e926ec770553b209a0
MacTwo Moylan <mactwomoylan@MacTwos-Mac-mini.local>
2014-07-01 13:18:34 +0800

    Initial Commit

---
+++ NetCare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetCare
+//
+//  Created by MacTwo Moylan on 7/1/14.
+//  Copyright (c) 2014 NetCare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
